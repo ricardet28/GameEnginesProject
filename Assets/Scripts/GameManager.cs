c57932b6430b95f9b76a2bbc61f81fe4292131d1
Ricky <lacajadericky@gmail.com>
2018-02-28 20:53:59 +0100

    creating gamemanager and levelmanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    private LevelManager _currentLevel;
+    private int[] scenesIndexArray = new int[] { 0, 1, 2, 3 };
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null && instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+        
+        SceneManager.LoadScene(1);
+        _currentLevel = GameObject.FindObjectOfType<LevelManager>();
+
+    }
+
+    private void Update()
+    {
+
+
+        _currentLevel = GameObject.FindObjectOfType<LevelManager>();
+        if (LevelCompleted())
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    private bool LevelCompleted()
+    {
+        return _currentLevel.levelCompleted;
+    }
+
+    private void SetCurrentLevel()
+    {
+        _currentLevel = GameObject.FindObjectOfType<LevelManager>();
+    }
+}
---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public int neededPoints;
+    public float maxTimeToComplete;
+    /*[HideInInspector]*/public bool levelCompleted;
+    public Vector3 spawnPoint;
+
+    private bool doorOpened;
+    private int currentPoints;
+    private float currentTime;
+    
+    private void Awake()
+    {
+        levelCompleted = false;
+        doorOpened = false;
+        currentPoints = 0;
+        currentTime = 0f;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private bool OpenDoor()
+    {
+        return currentPoints >= neededPoints && currentTime < maxTimeToComplete;
+    }
+
+}
