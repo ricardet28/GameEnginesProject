7f2bb3158402b412b5ec680ec94ae8ff1a653c82
oskarter9 <oscar5teruel@gmail.com>
2018-05-06 15:25:15 +0200

    level 1 casi completo

---
+++ Assets/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    public GameObject topPosition;
+    public GameObject basePosition;
+    private Transform _topElevator;
+    private Transform _baseElevator;
+    enum elevatorStates { goUp, goDown};
+    private elevatorStates states;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+        _topElevator = topPosition.transform;
+        _baseElevator = basePosition.transform;
+        states = elevatorStates.goUp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y >= _topElevator.position.y)
+            states = elevatorStates.goDown;
+        if (transform.position.y <= _baseElevator.position.y)
+            states = elevatorStates.goUp;
+        moveElevator();
+    }
+
+    void moveElevator()
+    {
+        if(states == elevatorStates.goUp)
+            transform.Translate(Vector3.up * Time.deltaTime * speed, Space.World);
+        if (states == elevatorStates.goDown)
+            transform.Translate(Vector3.down * Time.deltaTime * speed, Space.World);
+    }
+}
