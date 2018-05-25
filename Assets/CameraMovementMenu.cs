88bd27fd61f7b4426e45350db42396c7f7443ba4
Ricky <lacajadericky@gmail.com>
2018-05-25 17:58:52 +0200

    buttons

---
+++ Assets/CameraMovementMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementMenu : MonoBehaviour {
+    public float speedTranslate;
+    public Transform init;
+    public Transform end;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        this.transform.Translate(0f, 0f, speedTranslate * Time.deltaTime);
+        
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("End"))
+        {
+            this.transform.position = init.position;
+        }
+    }
+}
